3375b97c9c618d43e5189c21d000a1912f7146b0
Daniel Friesel <derf@finalrewind.org>
2012-04-29 13:16:15 +0200

    add cgi

---
+++ cgi/index.pl
@@ -0,0 +1,163 @@
+#!/usr/bin/env perl
+use Mojolicious::Lite;
+use File::Slurp qw(slurp);
+
+sub handle_request {
+	my $self  = shift;
+	my $type  = $self->stash('source') // q{};
+	my $state = $self->stash('state') // q{};
+
+	given ("$type/$state") {
+		when ('all/on')       { system('pgctl_on') }
+		when ('all/off')      { system('pgctl_off') }
+		when ('mains/on')     { system('pgctl mains_on') }
+		when ('mains/off')    { system('pgctl mains_off') }
+		when ('light/on')     { system('pgctl light_on') }
+		when ('light/off')    { system('pgctl light_off') }
+		when ('light/60p')    { system('pgctl light_60p') }
+		when ('light/40p')    { system('pgctl light_40p') }
+		when ('light/20p')    { system('pgctl light_20p') }
+		when ('light/10p')    { system('pgctl light_10p') }
+		when ('light/strobe') { system('pgctl light_strobe') }
+	}
+
+	$self->render( 'main', title => 'pgctl', );
+}
+
+helper show_link => sub {
+	my ( $self, $type, $state ) = @_;
+
+	my $state_is = slurp( "/tmp/.pgctl_${type}", { err_mode => 'quiet', } )
+	  // q{};
+	my $show_state = $state;
+	$show_state =~ s{p$}{%};
+
+	return sprintf(
+		'<a class="%s %s"  href="/%s/%s">%s</a>',
+		( $state =~ m{^\d} ) ? "l$state" : $state,
+		( $state eq $state_is ) ? 'cur' : q{},
+		$type, $state, $show_state,
+	);
+};
+
+app->config(
+	hypnotoad => {
+		accepts  => 10,
+		listen   => ['http://*:8092'],
+		pid_file => '/tmp/pgctl.pid',
+		workers  => 1,
+	},
+);
+
+get '/'               => \&handle_request;
+get '/:source/:state' => \&handle_request;
+
+app->start();
+
+__DATA__
+
+@@ main.html.ep
+<!DOCTYPE html>
+<html>
+<head>
+	<title><%= $title %></title>
+	<meta charset="utf-8">
+	<style type="text/css">
+
+	body {
+		font-family: Sans-Serif;
+		font-size: 300%;
+	}
+
+	div.break {
+		clear: both;
+	}
+
+	div.all,
+	div.mains,
+	div.light {
+		float: left;
+		text-align: center;
+		margin-left: 1em;
+		font-variant: small-caps;
+	}
+
+	div.desc {
+		width: 4em;
+		height: 2em;
+		text-align: center;
+		border-bottom: 1px dashed black;
+	}
+
+	div.all a,
+	div.mains a,
+	div.light a {
+		display: block;
+		width: 4em;
+		height: 3em;
+		text-align: center;
+		text-decoration: none;
+	}
+
+	a.on {
+		color: black;
+		background-color: white;
+	}
+
+	a.off {
+		color: white;
+		background-color: black;
+	}
+
+	a.l10p {
+		color: white;
+		background-color: #222222;
+	}
+
+	a.l20p {
+		color: white;
+		background-color: #333333;
+	}
+
+	a.l40p {
+		color: white;
+		background-color: #666666;
+	}
+
+	a.l60p {
+		color: black;
+		background-color: #bbbbbb;
+	}
+
+	a.strobe {
+		color: black;
+		background-color: yellow;
+	}
+
+	a.cur {
+		font-weight: bold;
+	}
+
+	</style>
+</head>
+<body>
+<div>
+<div class="light">
+<div class="desc">light</div>
+% for my $state (qw(on off 10p 20p 40p 60p strobe)) {
+%== show_link('light', $state)
+% }
+</div>
+<div class="mains">
+<div class="desc">mains</div>
+%== show_link('mains', 'on')
+%== show_link('mains', 'off')
+</div>
+<div class="all">
+<div class="desc">all</div>
+%== show_link('all', 'on')
+%== show_link('all', 'off')
+</div>
+</div>
+</body>
+</html>
